6e0927e916f4e9d99604b17a089e707c784103e7
Captain <enockbett427@gmail.com>
2024-10-10 15:36:40 +0300

    use my credentials

--- app/__init__.py
+++ app/__init__.py
@@ -45,9 +45,9 @@ def create_app(config_name):
         user_datastore.find_or_create_role(name='Member', description='Regular member')
         
         # Create Admin
-        if not user_datastore.find_user(email='enockbett427@gmail.com'):
+        if not user_datastore.find_user(email='captainbett77@gmail.com'):
             hashed_password = hash_password('123456')
-            user_datastore.create_user(email='enockbett427@gmail.com', password=hashed_password,
+            user_datastore.create_user(email='captainbett77@gmail.com', password=hashed_password,
                                        id_number=12345678, full_name='Cpt. Bett',
                                        roles=[user_datastore.find_role('Admin')])
             db.session.commit()
