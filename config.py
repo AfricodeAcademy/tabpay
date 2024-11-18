7cd0192ac7fd1bc38c6949f2c758dd7daad3239e
Benard Ronoh <biikate48@gmail.com>
2024-11-18 16:05:04 +0300

    Fixed config for database

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///tabpay.db')
 
 config = {
     'development': DevelopmentConfig,
