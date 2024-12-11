3febad9f13da13ee4ceac7e99f8fa804682acec7
Benard Ronoh <biikate48@gmail.com>
2024-12-11 18:38:59 +0300

    Made bank_id optional in payments table

--- app/__init__.py
+++ app/__init__.py
@@ -80,7 +80,7 @@ class CustomCsrfProtect(CSRFProtect):
             return None
             
         # Check if it's an M-Pesa callback
-        if request.path in ['/payments/confirmation', '/payments/validation', '/payments/stk/callback', '/api/v1/payments/stk/callback']:
+        if request.path in ['/payments/confirmation', '/payments/validation', '/payments/stk/callback', '/api/v1/payments/stk/callback', '/api/v1/payments']:
             return None
             
         # Default error handling
